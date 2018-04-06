6980d7d8549bb069181b786b44ea561956bb9aeb
Benjamin Klaus <klaus@technikum-wien.at>
2018-04-06 16:09:03 +0200

    changed autostart scripts, starting (1) roscore, (2) motor driver, (3) camera node. does not work all times, i do not know why. also added cameratest.py that tests the camera for 1 second

--- services/PiROSBotCam.sh
+++ services/PiROSBotCam.sh
@@ -1,26 +1,23 @@
 #! /bin/bash
-
-ROS_PORT=11311
-
 ROSLAUNCH_BIN="roslaunch"
+ROSLAUNCH_FILE="raspicam_node camerav2_640x480.launch"
 ROSMAINSOURCE="/opt/ros/kinetic/setup.bash"
 ROSSOURCE="/home/ros/catkin_ws/devel/setup.bash"
-ROSLAUNCH_FILE="raspicam_node camerav2_640x480.launch"
-
 
 echo "source ... "
 source $ROSMAINSOURCE
 source $ROSSOURCE 
 
-# set current IP for ROS network
+export ROS_PORT=11311
 export ROS_IP=$(hostname -I)
 export ROS_MASTER_URI=http://localhost:$ROS_PORT
 
-# wait for startup of PiROSBot Motor nodes
-sleep 10
-
+echo $ROS_PORT
 echo $ROS_IP
 echo $ROS_MASTER_URI
+
+echo "testing camera ..."
+python /home/ros/services/cameratest.py
+
 echo "starting ros camera ... "
 $ROSLAUNCH_BIN $ROSLAUNCH_FILE --port=$ROS_PORT
-
